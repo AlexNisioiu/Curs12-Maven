5de1c3d14d0041a96c644e32f3760c9a2bd44abe
Alex Nisioiu <alexnisioiu1@gmail.com>
2024-07-18 15:34:21 +0300

    curs 12 complet

---
+++ src/main/java/org/fasttrackit/MyFirstAnnotation.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD,ElementType.TYPE,ElementType.METHOD})
+public @interface MyFirstAnnotation {
+    int defaultAge() default 0;
+
+
+}
